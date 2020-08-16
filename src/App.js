336a43ea7ed10eca3500268bfa1e9108653d52ef
Maria <lu_vinas@hotmail.com>
2020-08-16 13:58:04 -0400

    form values changed

--- src/App.js
+++ src/App.js
@@ -8,29 +8,25 @@ class App extends Component {
       triviaCategory: 'Any Category',
       nbrOfQuestions:10
     };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
+
+  handleChange = (event) => {
     event.preventDefault();
 
     const target = event.target;
     const value = target.value;
-    // const name = target.name;
+    const name = target.name;
 
     this.setState ({
-      triviaCategory: value,
-      nbrOfQuestions: value
-      // [name]: value
+      [name]: value
     })
 
     console.log([value]);
   
   }
 
-  handleSubmit(event) {
+  handleSubmit = (event) => {
     event.preventDefault();
 
   }
@@ -44,31 +40,31 @@ class App extends Component {
             <label> Select Category:</label>
               <select name= "triviaCategory" className="form-control" value={this.state.triviaCategory} onChange={this.handleChange}>
                 
-                <option value="Any Category">Any Category</option>
-                <option value="General Knowledge">General Knowledge</option>
-                <option value="Entertainment: Books">Entertainment: Books</option>
-                <option value="Entertainment: Film">Entertainment: Film</option>
-                <option value="Entertainment: Music">Entertainment: Music</option>
-                <option value="Entertainment: Musicals &amp; Theatres">Entertainment: Musicals &amp; Theatres</option>
-                <option value="Entertainment: Television">Entertainment: Television</option>
-                <option value="Entertainment: Video Games">Entertainment: Video Games</option>
-                <option value="Entertainment: Board Games">Entertainment: Board Games</option>
-                <option value="Science &amp; Nature">Science &amp; Nature</option>
-                <option value="Science: Computers">Science: Computers</option>
-                <option value="Science: Mathematics">Science: Mathematics</option>
-                <option value="Mythology">Mythology</option>
-                <option value="Sports">Sports</option>
-                <option value="Geography">Geography</option>
-                <option value="History">History</option>
-                <option value="Politics">Politics</option>
-                <option value="Art">Art</option>
-                <option value="Celebrities">Celebrities</option>
-                <option value="Animals">Animals</option>
-                <option value="Vehicles">Vehicles</option>
-                <option value="Entertainment: Comics">Entertainment: Comics</option>
-                <option value="Science: Gadgets">Science: Gadgets</option>
-                <option value="Entertainment: Japanese Anime &amp; Manga">Entertainment: Japanese Anime &amp; Manga</option>
-                <option value="Entertainment: Cartoon &amp; Animations">Entertainment: Cartoon &amp; Animations</option>	
+                <option value="Any">Any Category</option>
+                <option value="9">General Knowledge</option>
+                <option value="10">Entertainment: Books</option>
+                <option value="11">Entertainment: Film</option>
+                <option value="12">Entertainment: Music</option>
+                <option value="13">Entertainment: Musicals &amp; Theatres</option>
+                <option value="14">Entertainment: Television</option>
+                <option value="15">Entertainment: Video Games</option>
+                <option value="16">Entertainment: Board Games</option>
+                <option value="17">Science &amp; Nature</option>
+                <option value="18">Science: Computers</option>
+                <option value="19">Science: Mathematics</option>
+                <option value="20">Mythology</option>
+                <option value="21">Sports</option>
+                <option value="22">Geography</option>
+                <option value="23">History</option>
+                <option value="24">Politics</option>
+                <option value="25">Art</option>
+                <option value="26">Celebrities</option>
+                <option value="27">Animals</option>
+                <option value="28">Vehicles</option>
+                <option value="29">Entertainment: Comics</option>
+                <option value="30">Science: Gadgets</option>
+                <option value="31">Entertainment: Japanese Anime &amp; Manga</option>
+                <option value="32">Entertainment: Cartoon &amp; Animations</option>	
             </select>
 
             <label>Number of Questions:</label>
