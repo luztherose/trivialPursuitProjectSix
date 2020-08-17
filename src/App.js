b79fdd8eeaa216301e30766fa6fa12d0001d3a23
Ryan Shao <ryan.shao95@gmail.com>
2020-08-17 15:49:28 -0400

    initial push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
+import NewGame from './components/NewGame';
+import SavedGames from './components/SavedGames';
 import axios from 'axios';
+import firebase from './firebase';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import "./App.css";
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <h1>Not So Trivial Pursuit</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <h1>Not So Trivial Pursuit</h1>
+          <Route path="/newGame" component = { NewGame } />
+          <Route path="/savedGames" component = { SavedGames } />
+          <nav>
+            <ul>
+              <button><Link to="/newGame">New Game</Link></button>
+              <button><Link to="/savedGames">Saved Games</Link></button>
+            </ul>
+          </nav>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/SavedGames.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SavedGames = (props) => {
+    return(
+        <div>
+            <h2>Check out the Saved Games!</h2>
+        </div>
+    )
+}
+
+export default SavedGames;
