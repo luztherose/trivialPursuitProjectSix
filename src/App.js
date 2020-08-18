0e0b3ce6073ea7524ec10f23cd179d8da895b8be
spencermdavis <spencermdavis@mac.com>
2020-08-18 17:07:56 -0400

    display function fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import React, { Component } from "react";
-import NewGame from './components/NewGame';
-import SavedGames from './components/SavedGames';
-import axios from 'axios';
-import firebase from './firebase';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import SavedGames from "./components/SavedGames";
+import axios from "axios";
+import firebase from "./firebase";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 import Form from "./components/Form";
+import GameCard from "./components/GameCard";
+import apiRequest from "./apiRequest";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      triviaCategory: "Any",
+      nbrOfQuestions: 10,
+      apiData: [],
+      gameName: "a great game",
+      gameCardReady: false,
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const amount = this.state.nbrOfQuestions;
+    const category = this.state.triviaCategory;
+
+    apiRequest.getQuestions(category, amount).then((res) => {
+      const actualState = this.state;
+      actualState.apiData = res.data.results;
+      this.setState({
+        actualState,
+        gameCardReady: true,
+      });
+      console.log(this.state.apiData);
+    });
+  };
 
   render() {
     return (
       <Router>
         <div className="App">
           <h1>Not So Trivial Pursuit</h1>
-          <Route path="/newGame" component = { Form } />
-          <Route path="/savedGames" component = { SavedGames } />
+          <Route
+            path="/newGame"
+            render={() => {
+              return (
+                <>
+                  <Form handleSubmit={this.handleSubmit} />
+                  {this.state.gameCardReady ? (
+                    <GameCard
+                      apiData={this.state.apiData}
+                      gameName={this.state.gameName}
+                    />
+                  ) : null}
+                </>
+              );
+            }}
+          />
+          <Route path="/savedGames" component={SavedGames} />
           <nav>
             <ul>
-              <button><Link to="/newGame">New Game</Link></button>
-              <button><Link to="/savedGames">Saved Games</Link></button>
+              <button>
+                <Link to="/newGame">New Game</Link>
+              </button>
+              <button>
+                <Link to="/savedGames">Saved Games</Link>
+              </button>
             </ul>
           </nav>
         </div>
--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,13 +1,12 @@
-import React from "react";
-import Button from "./button";
+import React, { Component } from "react";
+import Button from "./Button";
 import ScoreCard from "./ScoreCard";
 
 class GameCard extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       questionNumber: 0,
-      question: this.props.apiData[`${this.state.questionNumber}`],
       score: 0,
       questionCorrect: false,
     };
@@ -15,8 +14,9 @@ class GameCard extends Component {
 
   checkAnswer = (userAnswer) => {
     if (
-      userAnswer === this.props.apiData.correct_answer &&
-      this.state.questionNumber < this.props.apiData.length()
+      userAnswer ==
+        this.props.apiData[this.state.questionNumber].correct_answer &&
+      this.state.questionNumber < this.props.apiData.length
     ) {
       this.setState({
         questionNumber: this.state.questionNumber + 1,
@@ -24,8 +24,9 @@ class GameCard extends Component {
         questionCorrect: true,
       });
     } else if (
-      userAnswer !== this.props.apiData.correct_answer &&
-      this.state.questionNumber < this.props.apiData.length()
+      userAnswer !==
+        this.props.apiData[this.state.questionNumber].correct_answer &&
+      this.state.questionNumber < this.props.apiData.length
     ) {
       this.setState({
         questionCorrect: false,
@@ -39,16 +40,16 @@ class GameCard extends Component {
       question,
       incorrect_answers,
       correct_answer,
-    } = this.props.question;
+    } = this.props.apiData[this.state.questionNumber];
     const answers = [correct_answer].concat(incorrect_answers).sort();
     return this.state.questionCorrect === true ? (
       <article className="GameCard GameCardCorrect">
         <div className="cardTitle">
           <h2>{this.props.gameName}</h2>
-          <span>{difficulty} | </span>
+          <span>Question difficulty: {difficulty}</span>
           <ScoreCard
             score={this.state.score}
-            gameLength={this.props.apiData.length()}
+            gameLength={this.props.apiData.length}
           />
           <p>Correct!</p>
         </div>
@@ -58,7 +59,7 @@ class GameCard extends Component {
         <div className="answerSpace">
           {answers.map((answer, index) => {
             return (
-              <Button key={index} onClick={checkAnswer(answer)}>
+              <Button key={index} onClick={() => this.checkAnswer(answer)}>
                 {answer}
               </Button>
             );
@@ -72,7 +73,7 @@ class GameCard extends Component {
           <span>{difficulty} | </span>
           <ScoreCard
             score={this.state.score}
-            gameLength={this.props.apiData.length()}
+            gameLength={this.props.apiData.length}
           />
           <p>Waiting for correct answer...</p>
         </div>
@@ -82,7 +83,7 @@ class GameCard extends Component {
         <div className="answerSpace">
           {answers.map((answer, index) => {
             return (
-              <Button key={index} onClick={checkAnswer(answer)}>
+              <Button key={index} onClick={() => this.checkAnswer(answer)}>
                 {answer}
               </Button>
             );
